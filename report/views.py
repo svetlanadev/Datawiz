78d51291c6ae61d8ca549a00936e564ff9f5881a
Svetik <turistenok1@gmail.com>
2016-10-05 14:14:26 +0300

    display turnover using categories

--- report/views.py
+++ report/views.py
@@ -1,46 +1,65 @@
 from django.shortcuts import render
 from django.conf import settings
 import datetime
-
-
-# def index(request):
-#             result = settings.DW.get_products_sale(products = [2833024, 2286946, 'sum'],by='turnover',
-#                               shops = [305, 306, 318, 321],
-#                               date_from = datetime.date(2015, 8, 9),
-#                               date_to = datetime.date(2015, 9, 9),
-#                               interval = datawiz.WEEKS)
-#             return render(request, 'index.html', {'result' : result.to_html()})
-
+import pandas as pd
 
 def index(request):
+        #getting info about client
         client = settings.DW.get_client_info()
-        # [x.room_number() for x in people if check(x)]
-        products = [p['product_id'] for p in settings.DW.get_product().get('results', [])]
-        # product = settings.DW.get_product(products=['2894313', '2894313'])
-        print  '>>>>>>>>>>>>>',  products
-        products.append('sum')
-        print products
-        result = settings.DW.get_products_sale(products=products,
-                                               by='turnover',
-                                               date_from=datetime.date(2015, 8, 9),
-                                               date_to=datetime.date(2015, 8, 9),
-                                               )
-        # return render(request, 'index.html', {'client': client, 'product': product, 'result' : result.to_html()})
-        return render(request, 'index.html', {'client': client, 'result': result.to_html()})
+        #getting info about main category
+        category_get = [cat['category_id'] for cat in settings.DW.get_category().get('results')]
+        print category_get
+        turnover1_get = settings.DW.get_categories_sale(categories=category_get,
+                                                by='turnover',
+                                                date_from=datetime.date(2015, 11, 17),
+                                                date_to=datetime.date(2015, 11, 17)
+                                                )
+        turnover1 = turnover1_get.ix['2015-11-17', 'turnover']
 
+        turnover2_get = settings.DW.get_categories_sale(categories=category_get,
+                                                        by='turnover',
+                                                        date_from=datetime.date(2015, 11, 18),
+                                                        date_to=datetime.date(2015, 11, 18)
+                                                        )
+        turnover2 = turnover2_get.ix['2015-11-18', 'turnover']
 
+        rate_diff_d = ((turnover2 - turnover1) / turnover1) * 100
+        rate_diff = round(rate_diff_d, 2)
+        diff_d = turnover2 - turnover1
+        diff = round(diff_d, 2)
 
-# def index(request):
-#         result = settings.DW.get_products_sale(by='turnover',
-#                               date_from = datetime.date(2015, 8, 9),
-#                               date_to = datetime.date(2015, 8, 9),
-#                               )
-#         return render(request, 'index.html', {'result' : result.to_html()})
-
-# def f(x):
-#     return x[0] + x[1]
-# test_apply = df.apply(f, axis=1)
-
+        return render(request, 'index.html', {'client': client, 'turnover1': turnover1, 'turnover2': turnover2, 'rate_diff': rate_diff, 'diff': diff })
 
 def report(request):
-        return render(request, 'report.html')
+    return render(request, 'report.html')
+
+
+# # # [x.room_number() for x in people if check(x)]
+# products = [p['product_id'] for p in settings.DW.get_product().get('results', [])]
+# # product = settings.DW.get_product(products=['2894313', '2894313'])
+# # print  '>>>>>>>>>>>>>',  products
+# products.append('sum')
+# # print products
+# turnover1_get = settings.DW.get_products_sale(products=products,
+#                                               by='turnover',
+#                                               date_from=datetime.date(2015, 8, 9),
+#                                               date_to=datetime.date(2015, 8, 9),
+#                                               )
+# turnover1 = turnover1_get.ix['2015-08-09', 'sum']
+# turnover2_get = settings.DW.get_products_sale(products=products,
+#                                               by='turnover',
+#                                               date_from=datetime.date(2015, 11, 17),
+#                                               date_to=datetime.date(2015, 11, 17),
+#                                               )
+# turnover2 = turnover2_get.ix['2015-11-17', 'sum']
+#
+# rate_diff_d = ((turnover2 - turnover1) / turnover1) * 100
+# rate_diff = round(rate_diff_d, 2)
+# diff_d = turnover2 - turnover1
+# diff = round(diff_d, 2)
+# return render(request, 'index.html', {'client': client, 'turnover1': turnover1, 'turnover2': turnover2, 'rate_diff': rate_diff,
+#                    'diff': diff, 'p_cat': p_cat})
+
+
+    # categories_get = settings.DW.get_category()
+# # categories_get = settings.DW.get_category().get('results')[0].get('category_id')
